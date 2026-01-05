de09e1ec94e628674cf4fc37140f9dbd55782771
adrian <adrianciro@hotmail.es>
2026-01-05 18:05:11 +0100

    day8 part2

--- 2025/bin/day08.ml
+++ 2025/bin/day08.ml
@@ -1,41 +1,50 @@
+module IntegerSet = Set.Make (Int)
+
 let euclidean_dist (x1, y1, z1) (x2, y2, z2) =
   let dx = x1 -. x2 in
   let dy = y1 -. y2 in
   let dz = z1 -. z2 in
   sqrt ((dx *. dx) +. (dy *. dy) +. (dz *. dz))
 
-let make_groups junctions n_groups =
-  (* list of distances sorted in ascending order *)
-  let dists =
-    junctions
-    |> List.mapi (fun i junct1 ->
-        junctions
-        |> List.mapi (fun j junct2 -> (euclidean_dist junct1 junct2, i, j)))
-    |> List.flatten
-    |> List.filter (fun (d, _, _) -> d <> 0.0)
-    |> List.sort_uniq (fun (d1, f1, t1) (d2, f2, t2) ->
-        if f1 == t2 then 0 else compare (d1, f1, t1) (d2, f2, t2))
-  in
+(* with a tree type partition these would be much faster *)
+let merge_partitions partitions a b =
+  let min, max = if a > b then (b, a) else (a, b) in
+  Array.map_inplace (fun x -> if x = max then min else x) partitions
 
-  (* List.iter (fun (d, f, t) -> Printf.printf "(%f, %d, %d) " d f t) dists;
-  print_newline (); *)
+let count_partitions partitions =
+  partitions
+  |> Array.fold_left (fun set x -> IntegerSet.add x set) IntegerSet.empty
+  |> IntegerSet.cardinal
 
-  let merge_partitions partitions a b =
-    let min, max = if a > b then (b, a) else (a, b) in
-    Array.map_inplace (fun x -> if x = max then min else x) partitions
-  in
-
-  let rec aux dists partitions n_groups =
-    if n_groups == 0 then partitions
+let make_conns junctions dists n_conns =
+  let rec aux dists partitions n_conns =
+    if n_conns == 0 then partitions
     else
       let _, idx_from, idx_to = List.hd dists in
-      merge_partitions partitions partitions.(idx_from) partitions.(idx_to);
-      aux (List.tl dists) partitions (n_groups - 1)
+      if partitions.(idx_from) <> partitions.(idx_to) then
+        merge_partitions partitions partitions.(idx_from) partitions.(idx_to);
+      aux (List.tl dists) partitions (n_conns - 1)
   in
 
   let partitions = Array.init (List.length junctions) (fun i -> i) in
 
-  aux dists partitions n_groups
+  aux dists partitions n_conns
+
+let connect_all junctions dists =
+  let rec aux dists partitions =
+    let _, idx_from, idx_to = List.hd dists in
+    if
+      partitions.(idx_from) <> partitions.(idx_to)
+      && count_partitions partitions == 2
+    then List.hd dists
+    else (
+      merge_partitions partitions partitions.(idx_from) partitions.(idx_to);
+      aux (List.tl dists) partitions)
+  in
+
+  let partitions = Array.init (List.length junctions) (fun i -> i) in
+
+  aux dists partitions
 
 let () =
   let junctions =
@@ -47,7 +56,23 @@ let () =
         | _ -> raise (Failure "parse"))
   in
 
-  let groups = make_groups junctions 1000 in
+  (* list of (dinstance, from, to) sorted in ascending order *)
+  let dists =
+    junctions
+    |> List.mapi (fun i junct1 ->
+        junctions
+        |> List.mapi (fun j junct2 -> (euclidean_dist junct1 junct2, i, j)))
+    |> List.flatten
+    |> List.filter (fun (d, _, _) -> d <> 0.0)
+    |> List.sort_uniq (fun (d1, f1, t1) (d2, f2, t2) ->
+        if f1 == t2 && t1 == f2 then compare d1 d2
+        else compare (d1, f1, t1) (d2, f2, t2))
+  in
+
+  let groups = make_conns junctions dists 1000 in
+  let _, last_connf, last_connt = connect_all junctions dists in
+  let x1, _, _ = List.nth junctions last_connf in
+  let x2, _, _ = List.nth junctions last_connt in
 
   let counts =
     Array.fold_left
@@ -59,11 +84,8 @@ let () =
     |> List.sort (fun (_, c1) (_, c2) -> compare c2 c1)
   in
 
-  (* List.iter (fun (_, c) -> Printf.printf "%d " c) counts;
-  print_newline (); *)
-
   let (_, c1), (_, c2), (_, c3) =
     (List.hd counts, List.nth counts 1, List.nth counts 2)
   in
 
-  Printf.printf "Part1: %d\n" (c1 * c2 * c3)
+  Printf.printf "Part1: %d\nPart2: %.0f\n" (c1 * c2 * c3) (x1 *. x2)
